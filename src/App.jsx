69a8474adce499cb9b8f1406924842856f56ded4
jaydeb-ds <das294761@gmail.com>
2025-01-16 23:32:43 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Todo from './todo';
+import { useState } from 'react';
+
+function App() {
+
+  
+  return(
+    <div>
+          <Todo/>
+    </div>
+  )
+}
+
+export default App;
